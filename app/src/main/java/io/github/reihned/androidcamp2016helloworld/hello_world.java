5d59458a065e6910de129690c5d0b98bd4b9c43f
reihned <reihned@gmail.com>
2016-07-12 16:35:10 -0400

    fix

---
+++ app/src/main/java/io/github/reihned/androidcamp2016helloworld/hello_world.java
@@ -0,0 +1,13 @@
+package io.github.reihned.androidcamp2016helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class hello_world extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_world);
+    }
+}
